9c518ffabf9f25d5d65d42889379609cc3ba027a
hoangtran281220 <hoangtran654@gmail.com>
2025-03-19 16:15:43 +0700

    update studentclass

--- ScoreManage-main/ScoreManagement/Pages/AdminMenu/StudentClassesManage/Index.cshtml.cs
+++ ScoreManage-main/ScoreManagement/Pages/AdminMenu/StudentClassesManage/Index.cshtml.cs
@@ -19,17 +19,55 @@ namespace ScoreManagement.Pages.AdminMenu.StudentClassesManage
         {
             _context = context;
         }
-
+        public IList<Class> Classes { get; set; } = default!;
         public IList<StudentClass> StudentClass { get;set; } = default!;
 
+        [BindProperty(SupportsGet = true)]
+        public int? SelectedClassId { get; set; }
+        public IList<Student> Students { get; set; } = new List<Student>();
+
+        [BindProperty]
+        public int SelectedStudentId { get; set; }
         public async Task OnGetAsync()
         {
+
+
             if (_context.StudentClasses != null)
             {
-                StudentClass = await _context.StudentClasses
-                .Include(s => s.Class)
-                .Include(s => s.Student).ToListAsync();
+                Classes = await _context.Classes.ToListAsync();
+
+
+                if (SelectedClassId.HasValue)
+                {
+                    StudentClass = await _context.StudentClasses
+                        .Where(sc => sc.ClassId == SelectedClassId.Value)
+                        .Include(sc => sc.Student)
+                        .Include(sc => sc.Class)
+                        .ToListAsync();
+
+                    Students = await _context.Students
+                    .Where(s => !_context.StudentClasses.Any(sc => sc.StudentId == s.StudentId && sc.ClassId == SelectedClassId.Value))
+                    .ToListAsync();
+                }
+
             }
+        }
+
+        public async Task<IActionResult> OnPostAddStudentAsync()
+        {
+            if (SelectedClassId.HasValue && SelectedStudentId > 0)
+            {
+                var newStudentClass = new StudentClass
+                {
+                    ClassId = SelectedClassId.Value,
+                    StudentId = SelectedStudentId
+                };
+
+                _context.StudentClasses.Add(newStudentClass);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index", new { SelectedClassId });
         }
     }
 }
